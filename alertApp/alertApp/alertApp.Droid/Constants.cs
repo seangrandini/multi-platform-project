f1082163eae3b7514ca45c405af0c2900c74ee09
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2015-06-19 14:50:12 +0200

    progetto base

---
+++ alertApp/alertApp/alertApp.Droid/Constants.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace alertApp.Droid
+{
+    class Constants
+    {
+        public const string SenderID = "<GoogleProjectNumber>"; // Google API Project Number
+        public const string ListenConnectionString = "<Listen connection string>";
+        public const string NotificationHubName = "<hub name>";
+    }
+}
---
+++ alertApp/alertApp/alertApp.Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Gcm.Client;
+
+namespace alertApp.Droid
+{
+	[Activity (Label = "alertApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+        public static MainActivity instance;
+        private void RegisterWithGCM()
+        {
+            // Check to ensure everything's setup right
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            // Register for push notifications
+            Log.Info("MainActivity", "Registering...");
+            GcmClient.Register(this, Constants.SenderID);
+        }
+
+        protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new alertApp.App ());
+
+            RegisterWithGCM();
+        }
+	}
+}
+
